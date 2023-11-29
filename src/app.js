761e82b46cfb56762e8768d944b19db0b94fb9da
Fidela1 <fidelatuyizere4@gmail.com>
2023-11-29 09:20:57 +0200

    Connect to database

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const PORT = process.env.PORT || 4000;
+
+
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+.then (() =>{
+  console.log('Connected to the dadatabase!')
+})
 
