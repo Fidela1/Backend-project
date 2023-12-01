8a83cb75a898d44cb0387a2ac7327ba32f7aa94f
Fidela1 <fidelatuyizere4@gmail.com>
2023-12-01 13:46:08 +0200

    User signup

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,31 @@
+const User = require('../models/users');
+
+    const signup = async (req, res, next) => {
+        const user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+    
+        try {
+            const newUser = await user.save();
+            res.json({
+                status: 'success',
+                statusCode: 201,
+                data: {
+                    user: newUser
+                }
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({
+                status: 'error',
+                message: 'Internal Server Error'
+            });
+        }
+    };
+
+
+module.exports = {
+    signup,
+};
